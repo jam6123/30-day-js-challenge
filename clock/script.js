d28ef673464b7e4217641690a8bad5dc44e1d5d1
jam <jam6123@gmail.com>
2023-07-27 23:08:46 +0800

    change some lines

--- clock/script.js
+++ clock/script.js
@@ -1,23 +1,19 @@
 const secondHandContainer = document.querySelector('.second-hand-container')
-const minuteHand = document.querySelector('.clock__minute-hand')
-const hourHand = document.querySelector('.clock__hour-hand')
-
-/*  I just used this to set the position of secondhand's container
-    to make second hand accurate in real time   */
-const now = new Date()
-const secondDeg = (now.getSeconds() / 60) * 360 
-secondHandContainer.style.rotate = `${secondDeg}deg`
+const minuteHandContainer = document.querySelector('.minute-hand-container')
+const hourHandContainer = document.querySelector('.hour-hand-container')
 
 function setTime() {
     const now = new Date()
+
+    const secondDeg = (now.getSeconds() / 60) * 360 
+    secondHandContainer.style.rotate = `${secondDeg}deg`
     
     const minuteDeg = (now.getMinutes() / 60) * 360
-    minuteHand.style.rotate = `${minuteDeg}deg`
+    minuteHandContainer.style.rotate = `${minuteDeg}deg`
     
     const hourDeg = (now.getHours() / 12) * 360
-    hourHand.style.rotate = `${hourDeg}deg`
+    hourHandContainer.style.rotate = `${hourDeg}deg`
 }
-setInterval(setTime, 1000)
 
 // I call this to position the clock's hands immediately at first load
 setTime() 
